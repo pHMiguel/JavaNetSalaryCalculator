b474e34843d0d7523b6f1ce346fe06054bc340b3
Aphonso Miguel <aphonso99@gmail.com>
2026-02-06 11:50:53 -0300

    [Fix] Otimização do Algoritmo de IRRF

--- src/facades/IRRFCalculadora.java
+++ src/facades/IRRFCalculadora.java
@@ -4,27 +4,25 @@ public class IRRFCalculadora {
 	public Double getIRRF(Double salarioBruto, Double inss) {
 		Double baseCalculo;
 		Double reducao;
-		Double resultado = null;	
-		
-		if(inss < 607.20) {
-			baseCalculo = salarioBruto - 607.20;
-		} else {
-			baseCalculo = salarioBruto - inss;
-		}
-		
-
-		if(baseCalculo >= 5000.01 && baseCalculo <= 7350.00){
-			reducao = 978.62 - (0.133145 * salarioBruto);
-		}else {
-			reducao = 0.0;
-		}
+		Double resultado;	
 		
 		if(salarioBruto > 5000.00) {
-				resultado = (((baseCalculo - 4664.68)* 0.275) + 374.05) - reducao;
+			if(inss < 607.20) {
+				baseCalculo = salarioBruto - 607.20;
+			} else {
+				baseCalculo = salarioBruto - inss;
+			}
+
+			if(baseCalculo >= 5000.01 && baseCalculo <= 7350.00){
+				reducao = 978.62 - (0.133145 * salarioBruto);
+			}else {
+				reducao = 0.0;
+			}
+				
+			resultado = (((baseCalculo - 4664.68)* 0.275) + 374.05) - reducao;
 		} else {
 			resultado = 0.0;
 		}
-		
 		return resultado;
 	}
 }
